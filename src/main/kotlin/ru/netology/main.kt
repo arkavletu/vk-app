9e3b318f4004432edb471756dd424e7af4704cc6
Alina Prokofeva <whitestring@inbox.ru>
2022-02-10 01:21:15 +0300

    checked

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,24 +1,27 @@
 package ru.netology
+const val MINUTE = 60
+const val HOUR = MINUTE * 60
+const val DAY = HOUR * 24
+const val TWO_DAYS = DAY * 2
+const val THREE_DAYS = DAY * 3
 
 fun main(){
     val userName = "Dobby"
-    val secondsAgo = 2
+    val secondsAgo = TWO_DAYS
     val wasHere = agoToText(secondsAgo)
     val message = formattedMessage(userName, wasHere)
     println(message)
 }
 fun agoToText(time: Int): String{
     val minutes = time/60
-    val hours = time/3600
-    val minutesSpell = spellMinutes(minutes)
-    val hoursSpell = spellHours(hours)
+    val hours = minutes/60
 
  return  when (time){
        in 0..60 -> "только что"
-       in 61..60*59 -> "$minutes $minutesSpell назад"
-       in 60 * 60..24 * 60 * 59 -> "$hours $hoursSpell назад"
-       in 24 * 60 * 60..48 * 60 * 59 -> "сегодня"
-       in 48 * 60 * 60..72 * 60 * 60 -> "вчера"
+       in MINUTE until HOUR -> "$minutes ${spellMinutes(minutes)} назад"
+       in HOUR until DAY -> "$hours ${spellHours(hours)} назад"
+       in DAY until TWO_DAYS -> "сегодня"
+       in TWO_DAYS until THREE_DAYS -> "вчера"
        else -> "давно"
     }
 }
@@ -26,15 +29,15 @@ fun agoToText(time: Int): String{
 fun spellMinutes(time: Int): String {
     return when {
         time in 2..4 || time % 10 in 2..4 -> "минуты"
-        time == 1 || time % 10 == 1 && time != 11 -> "минуту"
+        time % 10 == 1 && time != 11 -> "минуту"
         else -> "минут"
     }
 }
 
 fun spellHours(time: Int): String {
-    return when (time){
-        1, 21 -> "час"
-        2, 4, 22, 23 -> "часа"
+    return when {
+        time % 10 == 1 && time != 11 -> "час"
+        time in 2..4 || time % 10 in 2..4 -> "часа"
         else -> "часов"
     }
 }
