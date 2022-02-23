2b3d506be193a11d47f97775415db4d52d02aa60
Alina Prokofeva <whitestring@inbox.ru>
2022-02-23 17:22:57 +0300

    checked

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,36 +1,42 @@
 package ru.netology
+
 const val MINUTE = 60
 const val HOUR = MINUTE * 60
 const val DAY = HOUR * 24
 const val TWO_DAYS = DAY * 2
 const val THREE_DAYS = DAY * 3
 
-fun main(){
+fun main() {
     val userName = "Dobby"
-    val secondsAgo = HOUR*4
+    val secondsAgo = MINUTE * 12
     val wasHere = agoToText(secondsAgo)
     println(formattedMessage(userName, wasHere))
 }
-fun agoToText(time: Int): String{
- return  when {
-     time >= THREE_DAYS -> "давно"
-     time >= TWO_DAYS -> "вчера"
-     time >= DAY-> "сегодня"
-     time >= HOUR -> "${spell(time, one ="час", two = "часа", other = "часов")} назад"
-     time >= MINUTE -> "${spell(time, one ="минуту", two = "минуты", other = "минут")} назад"
-     else -> "только что"
+
+fun agoToText(time: Int): String {
+    return when {
+        time >= THREE_DAYS -> "давно"
+        time >= TWO_DAYS -> "вчера"
+        time >= DAY -> "сегодня"
+        time >= HOUR -> "${spell(time, one = "час", two = "часа", other = "часов")} назад"
+        time >= MINUTE -> "${spell(time, one = "минуту", two = "минуты", other = "минут")} назад"
+        else -> "только что"
     }
 }
 
-fun spell(amount: Int, one: String, two: String, other: String): String{
-    val min = if(amount/MINUTE < 60) amount/MINUTE else amount/HOUR
-
-    return  when {
-        min % 10 == 1 && min != 11-> "$min $one"
-        min % 10 in 2..4 -> "$min $two"
-        else -> "$min $other"
+fun spell(amount: Int, one: String, two: String, other: String): String {
+    val timeAgo = convertSeconds(amount)
+    val remainder = timeAgo % 10
+    return when {
+        remainder == 1 && timeAgo != 11 -> "$timeAgo $one"
+        remainder in 2..4 && timeAgo != 12 -> "$timeAgo $two"
+        else -> "$timeAgo $other"
 
     }
+}
+
+fun convertSeconds(amount: Int): Int{
+    return if (amount < HOUR) amount / MINUTE else amount / HOUR
 }
 
 fun formattedMessage(userName: String, wasHere: String): String {
