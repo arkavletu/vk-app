1200eed8c5f96ae948de14984dc4ff4c4e437b80
Alina Prokofeva <whitestring@inbox.ru>
2022-03-01 23:59:14 +0300

    checked

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -8,19 +8,25 @@ const val THREE_DAYS = DAY * 3
 
 fun main() {
     val userName = "Dobby"
-    val secondsAgo = HOUR * 13
+    val secondsAgo = HOUR * 114
     val wasHere = agoToText(secondsAgo)
     println(formattedMessage(userName, wasHere))
 }
 
 fun agoToText(time: Int): String {
-    val convertedSeconds = convertSeconds(time)
+
     return when {
         time >= THREE_DAYS -> "давно"
         time >= TWO_DAYS -> "вчера"
         time >= DAY -> "сегодня"
-        time >= HOUR -> "$convertedSeconds ${spell(convertedSeconds, one = "час", two = "часа", other = "часов")} назад"
-        time >= MINUTE -> "$convertedSeconds ${spell(convertedSeconds, one = "минуту", two = "минуты", other = "минут")} назад"
+        time >= HOUR -> {
+            val hours = time / HOUR
+            "$hours ${spell(hours, one = "час", two = "часа", other = "часов")} назад"
+        }
+        time >= MINUTE -> {
+            val minutes = time / MINUTE
+            "$minutes ${spell(minutes, one = "минуту", two = "минуты", other = "минут")} назад"
+        }
         else -> "только что"
     }
 }
@@ -28,17 +34,16 @@ fun agoToText(time: Int): String {
 fun spell(amount: Int, one: String, two: String, other: String): String {
 
     val remainder = amount % 10
+    val remainder2 = amount % 100
     return when {
-        remainder == 1 && amount != 11 -> one
-        remainder in 2..4 && amount !in 12..14 -> two
+
+        remainder == 1 && remainder2 != 11 -> one
+        remainder in 2..4 && remainder2 !in 12..14 -> two
         else -> other
 
     }
 }
 
-fun convertSeconds(amount: Int): Int{
-    return if (amount < HOUR) amount / MINUTE else amount / HOUR
-}
 
 fun formattedMessage(userName: String, wasHere: String): String {
     return "$userName был(а) в сети $wasHere"
